4ce6171149d5982a8b664e76ab98238300355e94
Yufeng <xu.y.63@pg.com>
2024-03-21 09:45:36 +0800

    update comments

--- src/driver/uart.c
+++ src/driver/uart.c
@@ -4,22 +4,22 @@
 #include "uart.h"
 
 #define BAUD_0      250000
-#define UBRR_0      ((F_CPU)/(BAUD_0*8UL)-1)    // see spec sheet: buad rate generator
+#define UBRR_0      ((F_CPU)/(BAUD_0*8UL)-1)        // see spec sheet: buad rate generator
 
 uint8_t (*write_to_buffer)(uint8_t);
 
 void uart0_init(uint8_t (*callback)(uint8_t)) {
-    UBRR0L = (uint8_t)UBRR_0;                   // write lower byte
-    UBRR0H = (uint8_t)(UBRR_0 >> 8);            // write higher byte
+    UBRR0L = (uint8_t)UBRR_0;                       // write lower byte
+    UBRR0H = (uint8_t)(UBRR_0 >> 8);                // write higher byte
 
-    UCSR0A |= (1<<U2X0);                        // U2X0 bit seems to be 1 by default on Pro Mini 3.3V 8Mhz
-    UCSR0B |= (1 << RXEN0) | (1 << TXEN0);      // enable transmitter & receiver
-    UCSR0B |= (1 << RXCIE0);                    // enable RX Complete Interrupt
+    UCSR0A |= (1<<U2X0);                            // U2X0 bit seems to be 1 by default on Pro Mini 3.3V 8Mhz
+    UCSR0B |= (1 << RXEN0) | (1 << TXEN0);          // enable transmitter & receiver
+    UCSR0B |= (1 << RXCIE0);                        // enable RX Complete Interrupt
 }
 
 void uart0_transmit(uint8_t data) {
-    while (!(UCSR0A & (1<<UDRE0)));             // wait for empty transmit buffer
-    UDR0 = data;                                // put data into buffer
+    while (!(UCSR0A & (1<<UDRE0)));                 // wait for empty transmit buffer
+    UDR0 = data;                                    // put data into buffer
 }
 
 void uart0_puts(const char* message) {
