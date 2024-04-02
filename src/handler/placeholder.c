f3e7e467b95c1402397bb63abf2df78cdfa118f8
Yufeng <xu.y.63@pg.com>
2024-04-02 16:08:18 +0800

    more code

--- src/handler/placeholder.c
+++ src/handler/placeholder.c
@@ -12,6 +12,9 @@
 
 #define BUFFER_SIZE 255
 
+#define SYNC_BYTE           0x55
+#define START_BYTE          0x5C
+
 #define TIME_SETUP          3
 #define TIME_DISABLE_SYNC   5
 #define TIME_MESSAGE_LENGTH 10
@@ -32,7 +35,12 @@ static enum communication_state_t current_state;
 static enum communication_state_t next_state;
 
 volatile static uint8_t buffer_index = 0;
+
+/* variables used to flag events */
+volatile static uint8_t comm_device_detected = 0;
 static uint8_t count = 0;
+
+uint8_t message_buffer[BUFFER_SIZE] = {0};
 
 void rx_put(uint8_t data);
 
@@ -46,8 +54,6 @@ void communication_init() {
     current_state = COMM_STEP_1;
     next_state = COMM_STEP_1;
 }
-
-uint8_t message_buffer[BUFFER_SIZE] = {0};
 
 /*  a section performs actions similar to pon.c
     for power plug detection, RX pin should be configured as INPUT
@@ -99,18 +105,16 @@ void communication_task() {
             if (next_state != current_state) {
                 gpio_toggle_g();
             }
-
             break;
 
             case COMM_STEP_2:
-            // send sync byte (0x55), set up 20 ms timeout              // if timeout, exit super-state
+            // send sync byte (0x55), set up 20 ms timeout                      // if timeout, exit super-state
             if (last_state != current_state) {
-                uart0_send_byte(0x55);
+                uart0_send_byte(SYNC_BYTE);
             }
             else {
                 next_state = COMM_STEP_3;
             }
-
             break;
 
             case COMM_STEP_3:
@@ -141,21 +145,52 @@ void communication_task() {
 
             case COMM_STEP_5:
             // timeout of 100 ms
-            if (last_state != current_state) {
+            if (last_state != current_state) {                                  // entry
                 count = TIME_MESSAGE_LENGTH;
+
+                /* assume RX is already enabled and ready */
+
+
+
                 gpio_toggle_g();
             }
-            else {
+            else {                                                              // internal
                 if (--count == 0) {
                     next_state = COMM_STEP_6;
                 }
+                if (comm_device_detected == 1) {
+
+                }
+                /*  length byte received
+                */
+                if (0) {
+                    next_state = COMM_STEP_6;
+                }
             }
-            if (next_state != current_state) {
+            if (next_state != current_state) {                                  // exit
                 gpio_toggle_g();
             }
             break;
 
             case COMM_STEP_6:
+            if (last_state != current_state) {
+                count = /* message byte - 2 */;
+                gpio_toggle_g();
+            }
+            else {
+                if (--count == 0) {
+                    next_state = COMM_STEP_7;
+                }
+                /*  entire message is received, counted in systicks, exit this state
+                    need uart0 driver to check this, probably
+                */
+                if (0) {
+                    //
+                }
+            }
+            if (next_state != current_state) {
+                gpio_toggle_g();
+            }
             break;
 
             case COMM_STEP_7:
@@ -170,8 +205,17 @@ void communication_task() {
 }
 
 void rx_put(uint8_t data) {
-    // message_buffer[buffer_index] = data;
-    // buffer_index++;
+    message_buffer[buffer_index] = data;
+    buffer_index++;
 
-
+    /* how do i check the contents of buffer in synchronization with fsm */
+    switch (0 /* check fsm state */) {
+    case COMM_STEP_5:                                                           // start byte is checked
+    if (message_buffer[0] == START_BYTE) {
+        comm_device_detected =1 ;
+    }
+    if (message_buffer[1] >= 5) {                                               // emits "E_COM_MESSAGE_LENGTH_DETECT" in old design, exit step 5
+        //
+    }
+    }
 }
